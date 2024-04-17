5e36f9c86014d18352bac1d82906468871bf8db9
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-04-16 20:35:53 -0500

    updated files

---
+++ so-so/src/soso.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by so-so applications
+//-------------------------------
+
+#include "so-so/Application.h"
+
+// ----EntryPoint------------------
+#include "so-so/EntryPoint.h"
+//---------------------------------
