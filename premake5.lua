0b5ea381bab9231fd7bc4fa7d0ab2bcb73828ab4
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-04-19 15:41:20 -0500

    Add Premake build system

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "so-so"
+	architecture "x64"
+
+	configurations 
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "so-so"
+	location "so-so"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"SS_PLATFORM_WINDOWS",
+			"SS_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+	filter "configurations:Debug"
+		defines "SS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SS_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"so-so/vendor/spdlog/include",
+		"so-so/src"
+	}
+
+	links 
+	{
+		"so-so"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"SS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SS_DIST"
+		optimize "On"
