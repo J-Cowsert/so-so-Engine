60cf7006f1cb0304fc82d2bf80b60cf270a7c14b
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-04-20 18:52:26 -0500

    add Soso asserts

--- so-so/src/so-so/core.h
+++ so-so/src/so-so/core.h
@@ -9,7 +9,14 @@
 
 #else
 #error soso only supports Windows :(
-
 #endif // SS_PLATFORM_WINDOWS
+
+#ifdef SS_CORE_ASSERTS
+	#define HZ_ASSERT(x, ...) { if(!(x)) { {SS_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { {SS_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define SS_ASSERT(x, ...)
+	#define SS_CORE_ASSERT(x, ...)
+#endif // SS_CORE_ASSERTS
 
 #define BIT(x) (1 << x)
