d3b37d5058e4fdf32b8ae5258e37a762d86145d4
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-05-02 13:25:05 -0500

    Add layers to header file

--- so-so/src/soso.h
+++ so-so/src/soso.h
@@ -3,6 +3,7 @@
 // For use by so-so applications
 //-------------------------------
 #include "so-so/Log.h"
+#include "so-so/Layer.h"
 #include "so-so/Application.h"
 
 // ----EntryPoint------------------
