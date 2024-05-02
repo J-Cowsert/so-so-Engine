9176956488cdb68b1e91e9dc26d12641489220fa
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-05-02 13:27:35 -0500

    Add layer and layerstack to project

---
+++ so-so/src/so-so/Layer.cpp
@@ -0,0 +1,7 @@
+#include "sspch.h"
+#include "Layer.h"
+
+soso::Layer::Layer(const std::string& debugName) 
+	: m_DebugName(debugName) {
+
+}
---
+++ so-so/src/so-so/Layer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "core.h"
+#include "Events/Event.h"
+
+namespace soso {
+
+	class SOSO_API Layer {
+
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer() = default;
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+
+	protected:
+		const std::string& m_DebugName;
+	};
+}
+
+
+
