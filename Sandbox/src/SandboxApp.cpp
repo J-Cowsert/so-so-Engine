ad844256c02e2db6b64c3ba3bb6bfb6b3a70e0de
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-05-02 13:25:59 -0500

    Add example layer to client application

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,9 +1,27 @@
 #include <soso.h>
 
+class ExampleLayer : public soso::Layer {
+public:
+
+	ExampleLayer()
+		:Layer("Example") 
+	{
+	}
+
+	void OnUpdate() override {
+		SS_INFO("ExampleLayer::OnUpdate");
+	}
+
+	void OnEvent(soso::Event& event) override {
+		SS_TRACE("{0}", event);
+	}
+};
+
+
 class Sandbox : public soso::Application {
 public:
 	Sandbox() {
-
+		PushLayer(new ExampleLayer);
 	}
 
 	~Sandbox() {
