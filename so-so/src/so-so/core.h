5d71284167213293dc8ed00783aa4eb6e9422812
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-05-04 13:41:48 -0500

    Fix SOSO_ASSERTS: was never defined

--- so-so/src/so-so/core.h
+++ so-so/src/so-so/core.h
@@ -11,12 +11,18 @@
 #error soso only supports Windows :(
 #endif // SS_PLATFORM_WINDOWS
 
+#ifdef SS_DEBUG
+	#define SS_CORE_ASSERTS
+#endif
+
 #ifdef SS_CORE_ASSERTS
-	#define HZ_ASSERT(x, ...) { if(!(x)) { {SS_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { {SS_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define SS_ASSERT(x, ...) { if(!(x)) { SS_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define SS_CORE_ASSERT(x, ...) { if(!(x)) { SS_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
 	#define SS_ASSERT(x, ...)
 	#define SS_CORE_ASSERT(x, ...)
 #endif // SS_CORE_ASSERTS
 
 #define BIT(x) (1 << x)
+
+#define SS_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
