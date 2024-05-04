9bf862952af30295c0ef403bd019615a53435f82
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-05-04 13:40:49 -0500

    Make Application a singleton & clear GL buffer

--- so-so/src/so-so/Application.cpp
+++ so-so/src/so-so/Application.cpp
@@ -5,11 +5,18 @@
 
 #include "glad/glad.h"
 
-#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
-
 namespace soso {
 
+	#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
+	Application* Application::s_Instance = nullptr;
+
 	Application::Application() {
+
+		SS_CORE_ASSERT(!s_Instance, "Application already exists!");
+	
+		s_Instance = this;
+
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
@@ -20,10 +27,12 @@ namespace soso {
 
 	void Application::PushLayer(Layer* layer) {
 			m_LayerStack.PushLayer(layer);
+			layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay) {
 		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e) {
@@ -41,14 +50,19 @@ namespace soso {
 	}
 
 	void Application::Run() {
+
+		
 		
 		while (m_Running) {
-			m_Window->OnUpdate();
+
+			glClearColor(0, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
-		}
 
+			m_Window->OnUpdate();
+		}
 	}
 
 	bool Application::OnWindowClose(WindowCloseEvent& e) {
@@ -56,5 +70,4 @@ namespace soso {
 		m_Running = false;
 		return true;
 	}
-
 }
