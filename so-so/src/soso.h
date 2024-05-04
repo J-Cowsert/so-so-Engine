855df765a1a054462354265b290992339ee761b2
Henry Cowsert <cowsert.jamesh@gmail.com>
2024-05-04 15:34:07 -0500

    Add Input interface & Key/Mouse codes to client header file

--- so-so/src/soso.h
+++ so-so/src/soso.h
@@ -2,9 +2,14 @@
 
 // For use by so-so applications
 //-------------------------------
-#include "so-so/Log.h"
-#include "so-so/Layer.h"
 #include "so-so/Application.h"
+#include "so-so/Layer.h"
+#include "so-so/Log.h"
+
+#include "so-so/Input.h"
+#include "so-so/KeyCodes.h"
+#include "so-so/MouseCodes.h"
+
 #include "so-so/ImGui/ImGuiLayer.h"
 
 // ----EntryPoint------------------
