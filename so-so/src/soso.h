113701175b2a92b90dcebcaefaeaf23e3aa072cc
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2025-03-17 17:43:43 -0500

    Add rendering stuff to soso header

--- so-so/src/soso.h
+++ so-so/src/soso.h
@@ -1,7 +1,6 @@
 #pragma once
 
-// For use by so-so applications
-//-------------------------------
+//-----Core------------------------
 #include "so-so/Core/Application.h"
 #include "so-so/Core/Layer.h"
 #include "so-so/Core/Log.h"
@@ -12,6 +11,17 @@
 #include "so-so/Core/Timestep.h"
 
 #include "so-so/ImGui/ImGuiLayer.h"
+
+
+// ----Renderer--------------------
+#include "so-so/Renderer/Renderer.h"
+#include "so-so/Renderer/RenderCommand.h"
+
+#include "so-so/Renderer/Buffer.h"
+#include "so-so/Renderer/VertexArray.h"
+#include "so-so/Renderer/Shader.h"
+#include "so-so/Renderer/ShaderLibrary.h"
+#include "so-so/Renderer/Texture.h"
 
 // ----EntryPoint------------------
 #include "so-so/Core/EntryPoint.h"
