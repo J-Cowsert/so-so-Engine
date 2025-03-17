6b0411f30892a7bd3e90df681290f31cdd1a5e79
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2025-03-17 17:38:30 -0500

    Implement openGL framebuffer

---
+++ so-so/src/Platform/OpenGL/OpenGLFrameBuffer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "so-so/Renderer/FrameBuffer.h"
+
+namespace soso {
+
+	class OpenGLFrameBuffer : public FrameBuffer {
+	public:
+		OpenGLFrameBuffer(const FrameBufferConfig& config);
+		virtual ~OpenGLFrameBuffer();
+
+		void Invalidate();
+
+		void Bind() override;
+		void Unbind() override;
+
+		void Resize(uint32_t width, uint32_t height) override;
+		int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
+
+		void ClearAttachment(uint32_t attachmentIndex, int value) override;
+
+		uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override { SS_CORE_ASSERT(index < m_ColorAttachments.size(), "index < m_ColorAttachments.size()"); return m_ColorAttachments[index]; }
+
+		const FrameBufferConfig& GetConfig() const override { return m_Config; }
+
+	private:
+		uint32_t m_RendererID = 0;
+		FrameBufferConfig m_Config;
+
+		std::vector<FrameBufferTextureConfig> m_ColorAttachmentConfigs;
+		FrameBufferTextureConfig m_DepthAttachmentConfig = FrameBufferTextureFormat::None;
+
+		uint32_t m_DepthAttachment = 0;
+		std::vector<uint32_t> m_ColorAttachments;
+	};
+}
