b3f752700da8b07bc0f2529555d6f08cd20ae1d6
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2025-03-30 12:03:43 -0500

    Add unique identifier

---
+++ so-so/src/so-so/Core/UID.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "core.h"
+
+
+namespace soso {
+
+	// Unique ID - Randomly generated 64 bit integer.
+
+	class UID {
+	public:
+		UID();
+		explicit UID(uint64_t uid);
+
+		operator uint64_t () { return m_UID; }
+		operator const uint64_t () const { return m_UID; }
+
+	private:
+		uint64_t m_UID;
+	};
+}
+
+namespace std {
+
+	template<>
+	struct hash<soso::UID> {
+
+		std::size_t operator()(const soso::UID& uid) const {
+			
+			return uid;
+		}
+	};
+}
