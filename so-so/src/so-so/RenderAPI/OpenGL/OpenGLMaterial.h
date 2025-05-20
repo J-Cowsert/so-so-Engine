dca92b0183cb13208fdf174a253655e3d5ad1482
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2025-05-20 15:30:32 -0500

    Iterate on Material system: remove ownership of uniform buffer.

--- so-so/src/so-so/RenderAPI/OpenGL/OpenGLMaterial.h
+++ so-so/src/so-so/RenderAPI/OpenGL/OpenGLMaterial.h
@@ -8,12 +8,16 @@ namespace soso {
 	class OpenGLMaterial : public Material {
 	public:
 		OpenGLMaterial(const std::shared_ptr<Shader>& shader, const std::string& name);
+		OpenGLMaterial(std::shared_ptr<Material> material, const std::string& name);
 		virtual ~OpenGLMaterial() override;
 	
-		void Bind(); 
+		SS_FORCE_INLINE void Bind() override; // Material shouldn't be responsible for binding before the draw call
 	
 		std::shared_ptr<Shader> GetShader() const override { return m_Shader; }
-		uint8_t GetFlags() const override { return m_MaterialFlags; }
+		uint32_t GetFlags() const override { return m_MaterialFlags; }
+
+		void EnableFlag(MaterialFlag flag) { m_MaterialFlags |= (uint32_t)flag; }
+		void DisableFlag(MaterialFlag flag) { m_MaterialFlags &= ~(uint32_t)flag; }
 
 		void Set(const std::string& name, const float value) override;
 		void Set(const std::string& name, const int value) override;
@@ -23,7 +27,6 @@ namespace soso {
 		void Set(const std::string& name, const glm::mat2 value) override;
 		void Set(const std::string& name, const glm::mat3 value) override;
 		void Set(const std::string& name, const glm::mat4 value) override;
-
 		void Set(const std::string& name, const std::shared_ptr<Texture2D>& texture) override;
 	
 		template <typename T>
@@ -32,18 +35,21 @@ namespace soso {
 			const auto& info = FindUniformInfo(name);
 
 			SS_CORE_ASSERT(info, "Could not find info");
+			SS_CORE_ASSERT(sizeof(T) == info->GetSize(), "Size mismatch");
 			
 			if (!info) {
 				SS_CORE_WARN("Name: {0}", name);
 				return;
 			}
 
-			auto& buffer = m_UniformByteBuffer;
+			ByteBuffer& buffer = m_UniformByteBuffer;
 			buffer.Write(&value, info->GetSize(), info->GetOffset());
 
 			// Set dirty flag true so that the uniform is reset.
-			m_IsDirty = true;
+			m_IsDirty = true; // TODO: remove
 		}
+
+		// TODO: Getters
 
 	private:
 		const ShaderUniform* FindUniformInfo(const std::string& name);
@@ -53,13 +59,12 @@ namespace soso {
 	private:
 		std::shared_ptr<Shader> m_Shader;
 		std::string m_Name;
-		uint8_t m_MaterialFlags = (uint8_t)MaterialFlag::None;
+		uint32_t m_MaterialFlags = (uint32_t)MaterialFlag::None;
 		ByteBuffer m_UniformByteBuffer;
 		
 		std::map<uint32_t, std::shared_ptr<Texture2D>> m_Textures;
 
-		std::shared_ptr<UniformBuffer> m_UniformBuffer; // Probably a better solution would be to have the shader create and hold all the ubos
-		bool m_IsDirty = false; // Make sure to not reset data if not needed
-
+		std::shared_ptr<UniformBuffer> m_UniformBuffer; // sus - should I cache this?
+		bool m_IsDirty = false; // TODO: remove. Not currently doing anything useful
 	};
 }
