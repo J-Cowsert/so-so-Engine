52c1b2c67e67758c63c7c451574d59211bfac7f6
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2025-06-04 15:21:02 -0500

    Refactor ImGuiLayer API

--- so-so/src/so-so/ImGui/ImGuiLayer.h
+++ so-so/src/so-so/ImGui/ImGuiLayer.h
@@ -9,19 +9,14 @@
 namespace soso {
 
 	class ImGuiLayer : public Layer {
-	
 	public:
 		ImGuiLayer();
 		~ImGuiLayer();
 
 		void OnAttach() override;
 		void OnDetach() override;
-		void OnImGuiRender() override;
 
 		void Begin();
 		void End();
-		
-	private:
-		float m_Time = 0.0f;
 	};
 }
